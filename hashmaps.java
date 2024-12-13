71becddbce6cd271ebee083a1e943e3437e406f3
poorna30646 <thotipoornachandra@gmail.com>
2024-12-13 14:00:15 +0530

    Add files via upload

---
+++ hashmaps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class fire{
+    public static void main(String args[])
+    {
+        HashMap<String,String> a= new HashMap<>();
+        a.put("Sno","01");
+        a.put("SName","Poorna");
+        System.out.println(a);
+    }
+}
