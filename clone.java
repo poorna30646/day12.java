d72aec5f6bef1067f25ffd8017b535aa9b928af8
poorna30646 <thotipoornachandra@gmail.com>
2024-12-13 13:39:52 +0530

    Add files via upload

---
+++ clone.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {
+
+public static void main(String[] args) {
+
+//Accepting values into a LinkedList
+
+LinkedList<Integer> a = new LinkedList<>();
+
+Scanner sc = new Scanner(System.in);
+
+int size = sc.nextInt();
+
+for(int i=0;i<size;i++)
+
+{
+
+int s = sc.nextInt();
+
+a.addFirst(s);
+
+}
+LinkedList<Integer> b = new LinkedList<>();
+b=(LinkedList)a.clone();
+System.out.println(b);
+
+
+}
+}
